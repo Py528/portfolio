5ad6e72843e679a7593bc0596d356c1f400e87c4
Pranav Shinde <pranaav.shinde5280@gmail.com>
2025-12-07 11:03:46 +0530

    Deploy static build for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: "/portfolio",
-  output: "export",
+  /* config options here */
 };
 
 export default nextConfig;
